08ad0138677a20ae332354724ea5812a24e208af
qusseei <qusseei@users.noreply.github.com>
2021-04-19 16:39:13 +0800

    20210419

---
+++ ReadMe.txt
@@ -0,0 +1,38 @@
+维修机标准包对应子模块版本如下：
+2020-07-20  WIN_MW_1.0.60.0_005T01 SVN：1018
+ 
+ NewWXJ.exe	1.2.0.1015
+ IoDll.dll	1.0.0.6
+ WxjDog.exe	1.0.0.32
+ 
+2019-07-20  WIN_MW_1.0.60.0_004T01 SVN：806
+ 
+ NewWXJ.exe	1.2.0.806
+ IoDll.dll	1.0.0.6
+ WxjDog.exe	1.0.0.4
+ 
+2019-07-20  WIN_MW_1.0.60.0_003T01 SVN：711
+ 
+ NewWXJ.exe	1.2.0.710
+ IoDll.dll	1.0.0.6
+ WxjDog.exe	1.0.0.4
+
+
+2019-03-20  WIN_MW_1.0.60.0_002T01 SVN：544
+ 
+ NewWXJ.exe	1.2.0.543
+ IoDll.dll	1.0.0.6
+ WxjDog.exe	1.0.0.4
+
+
+2018-10-11  WIN_MW_1.0.60.0T01 SVN：193
+ 
+ NewWXJ.exe	1.2.0.211
+ ZCTDll.dll	1.7.0.254
+ Common.dll	1.3.0.185
+ NetMap.dll	1.0.2.191
+ IoDll.dll	1.0.0.6
+ WxjDog.exe	1.0.0.4
+
+
+
--- easycopy_v3.bat
+++ easycopy_v3.bat
@@ -1,9 +1,8 @@
 @echo off
 cls
-color 7d
+
 echo.
-echo easycopy v0.2 by aki 2020.09.11
-echo.
+echo easycopy v0.3 by aki 2021.4.19
 echo.
 echo Welcome To Try EasyCopy
 echo Copy The Data First,Then Compress
@@ -13,48 +12,69 @@ echo Type Enter To Created Save Path
 set /p Typestart=
 
 set wxjpath=E:\JD1AWXJ\
+set mylogserverpath=E:\MYLOGSERVER\
 setlocal enabledelayedexpansion
 for %%i in (%wxjpath%*.rar) do (set "stationame=%%~nxi")
 set abbreviation=%stationame:~0,3%
 set thisday=%date:~0,4%%date:~5,2%%date:~8,2%
-set string1=E:\
-set string2=\
-set thisday=%string1%%abbreviation%%thisday%%string2%
-if not exist %thisday% ( md %thisday% ) else (rd /s /q %thisday% && md %thisday% )
-echo %thisday% Has Been Created
+set current_directory=%~dp0
+set savepath=%currentdirectory%%abbreviation%%thisday%\
+if not exist %savepath% ( md %savepath% ) else (rd /s /q %savepath% && md %savepath% )
+echo %current_directory%%savepath% Has Been Created
 
 :judge
+echo 
 echo y:Copying Data;n:Compress Data? y/n
 set /p typechoice=
 if not %typechoice%==y goto compress
-echo Type Date(Such As 7_09,11_23)
-set /p typetime=
+
+echo Year (Such As 2020,2021)
+set /p year=
+echo Month(Such As 08,09,10,11)
+set /p month=
+echo Day(Such As 08,09,10,11)
+set /p day=
+set typetime=%month:~-1%_%day%
+set typetime1=%year%-%month%-%day%
 
 :copying
 echo "Copying Now"
 echo %wxjpath%
-xcopy %wxjpath%*.rar %thisday% /d /y
-xcopy %wxjpath%replays\replay%typetime%.* %thisday%\replays\ /s /e /d /y
-xcopy %wxjpath%alarms\alm%typetime%.* %thisday%\alarms\ /s /e /d /y
-xcopy %wxjpath%button\btn%typetime%.* %thisday%\button\ /s /e /d /y
-xcopy %wxjpath%errors\err%typetime%.* %thisday%\errors\ /s /e /d /y
-xcopy %wxjpath%sysinfo\sys*%typetime%.txt %thisday%\sysinfo\ /s /e /d /y
-if exist %thisday%replays\replay%typetime%.* goto judge else goto nodata
+xcopy %wxjpath%*.rar %savepath%JD1AWXJ\ /d /y
+xcopy %wxjpath%replays\replay%typetime%.* %savepath%JD1AWXJ\replays\ /s /e /d /y
+xcopy %wxjpath%alarms\alm%typetime%.* %savepath%JD1AWXJ\alarms\ /s /e /d /y
+xcopy %wxjpath%button\btn%typetime%.* %savepath%JD1AWXJ\button\ /s /e /d /y
+xcopy %wxjpath%errors\err%typetime%.* %savepath%JD1AWXJ\errors\ /s /e /d /y
+xcopy %wxjpath%sysinfo\sys*%typetime%.txt %savepath%JD1AWXJ\sysinfo\ /s /e /d /y
+
+echo %mylogserverpath%
+xcopy %mylogserverpath%*LOG*.rar %savepath%MYLOGSERVER\ /d /y
+xcopy %mylogserverpath%Data\*%typetime1%.* %savepath%MYLOGSERVER\Data\ /s /e /d /y
+xcopy %mylogserverpath%Log\*%typetime1%.* %savepath%MYLOGSERVER\Log\ /s /e /d /y
+
+if exist %savepath%replays\replay%typetime%.* goto judge else goto nodata
 
 :nodata
-echo No Data For Thisday
+echo No Data For savepath
 goto judge
 
 :compress
 echo Type Enter To Start Unpack
 set /p unpack=
-start winrar x -r -ikbc -inul %thisday%*mw*.rar *.* %thisday%
+start winrar x -y -r- -ikbc -inul %savepath%JD1AWXJ\*MW*.rar *.* %savepath%JD1AWXJ\
+start winrar x -y -r- -ikbc -inul %savepath%MYLOGSERVER\*LOG*.rar *.* %savepath%MYLOGSERVER\
 
 echo Type Enter To Create Compressed Package
 set /p continue=
-start winrar m -r -agyyyymmdd-nn -ep1 -inul %thisday%%typetime%__.rar %thisday%\*
+start winrar m -y -r -ep1 -inul %savepath%JD1AWXJ-%year%%month%%day%.rar %savepath%JD1AWXJ\*
+start winrar m -y -r -ep1 -inul %savepath%MYLOGSERVER-%year%%month%%day%.rar %savepath%MYLOGSERVER\*
 
 echo Wait To Complete
 
 pause
+
+if exist %savepath%JD1AWXJ\ ( rd /s /q %savepath%JD1AWXJ\ ) else (echo  )
+if exist %savepath%MYLOGSERVER\ ( rd /s /q %savepath%MYLOGSERVER\ ) else (echo  )
+
 @echo on
+
